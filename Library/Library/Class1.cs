be2205dfbf29310c925be5e46a7cb062b543fa7d
Name <korikrom@gmail.com>
2025-04-30 13:39:45 +0300

    Initial

---
+++ Library/Library/Class1.cs
@@ -0,0 +1,12 @@
+﻿namespace Library;
+
+/// <summary>
+/// Математика.
+/// </summary>
+public class MathHelper
+{
+    /// <summary>
+    /// Складывает два числа.
+    /// </summary>
+    public int Add(int a, int b) => a + b;
+}
